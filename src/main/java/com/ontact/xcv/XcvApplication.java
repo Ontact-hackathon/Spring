e5873072c229504c368cad303f40dd8dd47056c2
soosungp33 <soosungp33@gmail.com>
2021-05-01 15:54:02 +0900

    first commit

---
+++ src/main/java/com/ontact/xcv/XcvApplication.java
@@ -0,0 +1,13 @@
+package com.ontact.xcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XcvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ontact/xcv/XcvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontact.xcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
